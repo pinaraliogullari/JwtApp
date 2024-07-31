0c79361a9a058db94f65dd8907ef5b24b8fa2926
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-07-31 16:36:19 +0300

    Product created.

--- JwtApp/JwtApp.Front/Controllers/ProductController.cs
+++ JwtApp/JwtApp.Front/Controllers/ProductController.cs
@@ -1,7 +1,8 @@
 ﻿using JwtApp.Front.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text;
 using System.Text.Json;
 
 namespace JwtApp.Front.Controllers
@@ -48,6 +49,59 @@ namespace JwtApp.Front.Controllers
             }
 
             return RedirectToAction("List");
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var model = new ProductCreateModel();
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
+            if (token != null)
+            {
+                var client = _httpClientFactory.CreateClient("ApiService1");
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                var response = await client.GetAsync($"api/Categories");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonData = await response.Content.ReadAsStringAsync();
+                    var data = JsonSerializer.Deserialize<List<CategoryListModel>>(jsonData, new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    });
+                    model.Categories = new SelectList(data, "Id", "Definition");
+                    return View(model);
+                }
+            }
+            return RedirectToAction("List");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductCreateModel model)
+        {
+            var data = TempData["Categories"]?.ToString();
+            if (data != null)
+            {
+                var categories = JsonSerializer.Deserialize<List<SelectListItem>>(data);
+                model.Categories = new SelectList(categories, "Value", "Text");
+            }
+            if (ModelState.IsValid)
+            {
+                var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
+                if (token != null)
+                {
+                    var client = _httpClientFactory.CreateClient("ApiService1");
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+                    var JsonData = JsonSerializer.Serialize(model);
+                    var content = new StringContent(JsonData, Encoding.UTF8, "application/json");
+
+                    var response = await client.PostAsync("api/Products", content);
+                    if (response.IsSuccessStatusCode)
+                        return RedirectToAction("List");
+                    ModelState.AddModelError("", "Bir hata oluştu");
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ JwtApp/JwtApp.Front/Models/ProductCreateModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace JwtApp.Front.Models
+{
+    public class ProductCreateModel
+    {
+        [Required(ErrorMessage ="Ürün adı boş geçilemez.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Stok boş geçilemez.")]
+        public int Stock { get; set; }
+
+        [Required(ErrorMessage = "Fiyat boş geçilemez.")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Kategori seçilmelidir.")]
+        public int CategoryId { get; set; }
+        public SelectList Categories { get; set; }
+    }
+}
