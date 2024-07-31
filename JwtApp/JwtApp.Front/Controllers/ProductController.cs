4dab62d7995b8b7f51d54f77181c3bef2924201b
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-07-31 15:30:38 +0300

    Products listed.

---
+++ JwtApp/JwtApp.Front/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using JwtApp.Front.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace JwtApp.Front.Controllers
+{
+    [Authorize(Roles = "Admin,Member")]
+    public class ProductController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
+            if (token != null)
+            {
+                var client = _httpClientFactory.CreateClient("ApiService1");
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                var response = await client.GetAsync("api/Products");
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonData = await response.Content.ReadAsStringAsync();
+                    var result = JsonSerializer.Deserialize<List<ProductListModel>>(jsonData, new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    });
+                    return View(result);
+                }
+            }
+            return View();
+        }
+    }
+}
